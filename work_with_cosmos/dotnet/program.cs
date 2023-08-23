d9fe3c580ed16a32315ec5811a8b63c730da5b44
Navaneeth Rajagopalan <navaneeth.rajagopalan@propella.ai>
2023-08-23 15:13:03 +1000

    Updated to communicate with cosmos DB

--- work_with_cosmos/dotnet/program.cs
+++ work_with_cosmos/dotnet/program.cs
@@ -4,37 +4,47 @@ using Microsoft.Azure.Cosmos;
 namespace PracticeCosmos
 {
 
-class Program
+    class Program
     {
-    static async Task Main(string[] args)
-        {   
+        static async Task Main(string[] args)
+        {
 
             // Note, the object names in the code can be whatever you want, but using the ones suggested
             //  will make it somewhat easier to follow in the video walkthrough and lab guide.
             //  However, use the *exact* names given for the database and container resources you will
             //  create in order to a) Help the lab grading engine properly indicate lab completion, and 
             //  b) Help keep your .NET work separate from your Python work, should you decide to do both.
-            
+
             //Copy the primary connection string from the Cosmos DB account in the portal and paste it in place of the placeholder, below
-            string my_connection_string = "[PASTE CONNECTION STRING, WRAPPED IN DOUBLE-QUOTES, HERE]";
+            string my_connection_string = "https://pluralsightlab-776-af3.documents.azure.com:443/";
 
             //Declare a CosmosClient, called myClient, using the connection string
-            
-            
+            CosmosClient myClient = new CosmosClient(connectionString: my_connection_string);
+            Database myDatabase = await myClient.CreateDatabaseIfNotExistsAsync("LabDBNet");
+            Container myContainer = await myDatabase.CreateContainerIfNotExistsAsync("LabItemsNet", "/labPK");
+
+            GenericItem myItem = new(
+              id: "70b63682-b93a-4c77-aad2-65501347265f",
+              itemName: "This is my string",
+              labPK: "Springfield"
+            );
+
+            GenericItem upsertItem = await myContainer.UpsertItemAsync<GenericItem>(myItem, new PartitionKey(myItem.labPK));
+
 
 
             //Asynchronously create a Database, called myDatabase, with a resource Id/name of *exactly* "LabDBNet";
             //Requirement: Attempt to create the database only if it does not already exist.
             //Hints: The code for this code and most of the other commands that follow will be in this form:
             //      Class myClass = await myPreviousObject.Create{Class}IfNotExistsAsync({one or more properties});
-            
-            
+
+
 
             //Asynchronously create a Container, called myContainer, on myDatabase, if it does not already exist,
             //  passing in the *exact* name of "LabItemsNet" and a partition key of *exactly* "/labPK"
-            
-            
-            
+
+
+
             //Use the GenericItem record (just outside of the Main method) to create an item object, 
             //  called myItem, to be upserted to the container.
             //IMPORTANT: the id property should be assigned a unique value, such as a GUID, like this:
@@ -43,16 +53,16 @@ class Program
             //    partition key value — perhaps, a city name, such as "Springfield" or "New Delhi" — 
             //    though, there are no actual constraints preventing labPK from being any value you choose.
 
-            
-                       
-           // Declare a GenericItem to asynchronously upsert myItem to myContainer.
-           
 
-    } 
-    public record GenericItem (
-    string id,
-    string itemName,
-    string labPK 
-    );   
-  }
-}       
+
+            // Declare a GenericItem to asynchronously upsert myItem to myContainer.
+
+
+        }
+        public record GenericItem(
+        string id,
+        string itemName,
+        string labPK
+        );
+    }
+}
